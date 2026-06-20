6a206ed49e2cf2da21e647fe4fd15a4e521bfb9c
dafe <dafemoritz@gmail.com>
2026-06-20 14:56:07 +0200

    test automatic deployment

--- node-cicd-project/app.js
+++ node-cicd-project/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from GitHub Actions CI/CD Pipeline 🚀');
+  res.send('Hello from GitHub Actions CI/CD Pipeline November Cohort Group 3🚀');
 });
 
 app.listen(3000, () => {
